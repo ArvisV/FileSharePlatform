06122ac34acbe6b281910f4e77b7d2d8fcd16553
Arvis Volants <oicislv@gmail.com>
2026-03-27 08:39:47 +0200

    Pievienots User modulis

--- obj/Debug/net10.0/FileSharePlatform.AssemblyInfo.cs
+++ obj/Debug/net10.0/FileSharePlatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileSharePlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b22613889c9428b9755c673eef4b2b916f06c057")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce696c37372eb7393b071db9334547a50968056")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileSharePlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileSharePlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
